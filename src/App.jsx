209bd2f323ae998d78693e4265bf798636b9dd21
codeinx <aseyeged@gmail.com>
2025-12-27 14:24:45 +0100

    Enhance Card component with click counter and like status logging

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 const Card = ({title}) => {
+  const [count, setCount] = useState(0);
   const [hasLiked, setHasLiked] = useState(false);
 
+  
 
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`);
+  }, [hasLiked]);
 
   return (
-    <div className='card'>
-      <h2>{title}</h2>
+    <div className='card' onClick={() => setCount((prevState) => prevState +1)}>
+      <h2>{title} <br/> {count}</h2>
       <button onClick={() => setHasLiked(!hasLiked)}>
         {hasLiked ? 'Liked' : 'Like'}
       </button>
