112b64e1e67d088f6b63fa6bc1cb2dc36be51c15
codeinx <aseyeged@gmail.com>
2025-12-29 09:07:07 +0100

    Integrate Tailwind CSS and update App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,8 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-const Card = ({title}) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-
-  const handleLike = () => {
-      if (hasLiked) {
-        setCount((c) => c - 1);
-      } else {
-        setCount((c) => c + 1);
-      }
-      setHasLiked(!hasLiked);
-  };
-
-  useEffect(() => {
-    console.log(`${title} has been liked: ${hasLiked}`);
-  }, [hasLiked]);
-
-  return (
-    <div className='card'>
-      <h2>{title} <br/> {count || null}</h2>
-      <button onClick={handleLike}>
-        {hasLiked ? 'Liked' : 'Like'}
-      </button>
-    </div>
-  )
-}
+import React from 'react'
 
 const App = () => {
-  
   return (
-    <div className='card-container'>
-      <Card title="Star Wars" />
-      <Card title="Avatar" />
-      <Card title="The Lion King" />
-    </div>
+    <div>App</div>
   )
 }
 
